7f02fd735d6aeb60feffc0843c57cafbf9ffa5b4
QuentinDll <quentin.delplace13@gmail.com>
2018-10-08 10:19:57 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="assets/css/style.css">
+        <title>Exercice 2</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-dark bg-dark">
+            <h1>Exercice 2</h1>
+            <a href="http://exephpp9/">Index</a>
+        </nav>
+        <p>
+            <?php
+            $date = date("d-m-Y");
+            Print($date);
+            ?>
+        </p>
+    </body>
+</html>
